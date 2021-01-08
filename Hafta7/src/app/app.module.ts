a25073a8c14cc88bc4c12ada5e34b9963f2a0d02
furkanogur <20191132035@ogr.akdeniz.edu.tr>
2021-01-09 00:06:35 +0300

    hafta7 v.1.0.0

--- Hafta7/src/app/app.module.ts
+++ Hafta7/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { Test1Component } from './components/test1/test1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Test1Component
   ],
   imports: [
     BrowserModule,
