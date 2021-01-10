7a91928bc7c50c11596584c135afae12d894f377
furkanogur <20191132035@ogr.akdeniz.edu.tr>
2021-01-10 14:34:25 +0300

    Hafta7 v2.0.0

--- Hafta7/src/app/components/home/home.component.ts
+++ Hafta7/src/app/components/home/home.component.ts
@@ -16,10 +16,10 @@ export class HomeComponent implements OnInit {
     public servis: DataService
   ) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.KayitListele();
     this.secKayit.id = 0;
-    
+
   }
 
   KayitListele() {
@@ -27,35 +27,57 @@ export class HomeComponent implements OnInit {
   }
 
   Kaydet() {
-    var tarih =new Date();
-    if (this.secKayit.id==0) {
-      this.secKayit.id =Math.floor(Math.random()*1000);
-      this.secKayit.kayTarih=tarih.getTime().toString();
-      this.secKayit.duzTarih=tarih.getTime().toString();
+    var tarih = new Date();
+    if (this.secKayit.id == 0) {
+      this.secKayit.id = Math.floor(Math.random() * 1000);
+      this.secKayit.kayTarih = tarih.getTime().toString();
+      this.secKayit.duzTarih = tarih.getTime().toString();
 
-      this.servis.KayitEkle(this.secKayit).subscribe(d =>{
-        this.sonuc.islem =true;
-        this.sonuc.mesaj ="Kayıt Eklendi..";
+      this.servis.KayitEkle(this.secKayit).subscribe(d => {
+        this.sonuc.islem = true;
+        this.sonuc.mesaj = "Kayıt Eklendi..";
         this.KayitListele();
-        this.secKayit= new Kayit();
-        this.secKayit.id=0;
-      },err=>{
-        this.sonuc.islem =false;
-        this.sonuc.mesaj ="Hata Oluştu..";
+        this.Vazgec();
+      }, err => {
+        this.sonuc.islem = false;
+        this.sonuc.mesaj = "Hata Oluştu..";
       });
     }
-    else{
+    else {
+      this.secKayit.duzTarih = tarih.getTime().toString();
+      this.servis.KayitDuzenle(this.secKayit).subscribe(d => {
+        this.sonuc.islem = true;
+        this.sonuc.mesaj = "Kayıt Düzenlendi..";
+        this.KayitListele();
+        this.Vazgec();
+      }, err => {
+        this.sonuc.islem = false;
+        this.sonuc.mesaj = "Hata Oluştu..";
+      });
 
     }
 
   }
 
-  Duzenle() {
-
+  Duzenle(kayit: Kayit) {
+    Object.assign(this.secKayit, kayit);
   }
 
-  Sil() {
+  Sil(kayit: Kayit) {
+    this.servis.KayitSil(kayit.id).subscribe(d => {
+      this.sonuc.islem = true;
+      this.sonuc.mesaj = "Kayıt Silindi..";
+      this.KayitListele();
+      this.Vazgec();
+    }, err => {
+      this.sonuc.islem = false;
+      this.sonuc.mesaj = "Hata Oluştu..";
+    });
+  }
 
+  Vazgec() {
+    this.secKayit = new Kayit();
+    this.secKayit.id = 0;
   }
 
 }
